094fcfbb5dda418cbea8151556dc3303de85abe8
abdelrahmansaqr929-spec <abdelrahmansaqr929@gmail.com>
2026-09-21 06:58:31 +0300

    answer_01

--- Program.cs
+++ Program.cs
@@ -4,6 +4,12 @@
     {
         static void Main(string[] args)
         {
+            #region answer_1
+            // Abstraction is the OOP principle of hiding complex implementation details and exposing only the essential features that a user of an object needs
+            // It lets you focus on what an object does rather than how it does it.
+            //Abstraction is considered a pillar because it addresses the central problem OOP was created to solve: managing complexity in large software systems
+            //The other three pillars (encapsulation, inheritance, polymorphism) are largely the means by which abstraction is put into practice
+            #endregion
 
         }
     }
