27abe009f611cd94b02a6c9b0937ec3fd3a498ad
abdelrahmansaqr929-spec <abdelrahmansaqr929@gmail.com>
2026-09-21 07:37:49 +0300

    answer_02

--- Program.cs
+++ Program.cs
@@ -10,6 +10,13 @@
             //Abstraction is considered a pillar because it addresses the central problem OOP was created to solve: managing complexity in large software systems
             //The other three pillars (encapsulation, inheritance, polymorphism) are largely the means by which abstraction is put into practice
             #endregion
+            #region answer_02
+            //Abstraction is considered a pillar because it addresses the central problem OOP was created to solve: managing complexity in large software systems
+            //The other three pillars(encapsulation, inheritance, polymorphism) are largely the means by which abstraction is put into practice
+            // choose an interface when you're defining a capability (what something can do),
+            // and an abstract class when you're defining a base type(what something is) that shares state or code.
+            //Multiple abstract class : No , sMultiple interfaces: yes
+            #endregion
 
         }
     }
